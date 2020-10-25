f8a163103f01ceca42a7c1f92ceb7b5afe8932e8
drebollo20.github.io <drebollo16@byui.edu>
2020-10-24 23:44:29 -0600

    changes

--- week06/todo.js
+++ week06/todo.js
@@ -7,6 +7,25 @@ function addButton() {
     newText.appendChild(inputText)
     document.getElementById('list').appendChild(newText);
 
+
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function(cklist) {
+        if (cklist.target.tagName == 'LI') {
+            cklist.target.classList.toggle('checked');
+        }
+    }, false);
+
+
+}
+
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
 }
 
 function deletetext() {
