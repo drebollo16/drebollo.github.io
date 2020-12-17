7359f9109a01759142167016d7aaae3e344da15b
drebollo16 <drebollo16@byui.edu>
2020-12-16 23:13:07 -0700

    changes

--- finalProject/js/recipeAPI.js
+++ finalProject/js/recipeAPI.js
@@ -1,6 +1,8 @@
-/*const API_KEY = "f87c22e3acf9b62024822b1b938e37d0";
+//const API_KEY = "f87c22e3acf9b62024822b1b938e37d0";
 const API_ID = "5e7124ab";
 
+
+const API_KEY = "a48000fcb94995b383b79cdebbf16765";
 const api_URL = `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
 
 async function getData() {
@@ -12,14 +14,7 @@ async function getData() {
     }).join(' ')
     console.log(html);
 
-    //document.querySelector('#app').insertAdjacentHTML("afterbegin", html);
-
     document.getElementById('app').textContent = html;
-    // document.getElementById('recipe').textContent = label;
-
-    // document.getElementById('ingrident').textContent = ingridents;
-    //document.getElementById('picture').textContent = image;
-    //document.getElementById('picture').textContent = hits;
 
 
     console.log(data.hits);
@@ -29,8 +24,8 @@ async function getData() {
 
 }
 getData();
- 
 
+/*
 ////////////////////////////
 fetch("https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=3&calories=591-722&health=alcohol-free");
 res => {
@@ -45,36 +40,6 @@ res => {
             });
         }
     }
-}*/
-const requestURL = 'https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=3&calories=591-722&health=alcohol-free';
+}
 
-fetch(requestURL)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(jsonObject) {
-
-        const recipes = jsonObject['hits'];
-        recipes.forEach(recipes => {
-
-            let card = document.createElement('section');
-
-            let label = document.createElement('h2');
-            let ingridents = document.createElement('div');
-            let image = document.createElement('img');
-
-
-            h2.innerHTML = `${recipes.hits}`;
-
-            image.setAttribute('src', recipes.imageurl);
-            h2.innerHTML = `<strong>Name: ${recipes.label}</strong>`;
-            ingridents.innerHTML = `<strong>Ingridents: ${recipes.ingridents}</strong>`;
-
-
-            card.appendChild(label);
-            card.appendChild(ingridents);
-            card.appendChild(image);
-
-            document.querySelector('div.app').appendChild(card);
-        });
-    });
+ */
